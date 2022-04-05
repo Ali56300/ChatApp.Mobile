725751cba8d39b2014305b33ed2096af0f873fd4
aliha <aliha@ALISDESKTOP>
2022-04-05 22:19:41 +0100

    Add project files.

---
+++ ChatApp.Mobile/ChatApp.Mobile/Services/Interfaces/IChatService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.Mobile.Services.Interfaces
+{
+    public interface IChatService
+    {
+        Task Connect();
+        Task Disconnect();
+        Task SendMessage(string userId, string messgae);
+        void ReceiveMessage(Action<string, string> GetMessageAndUser); 
+
+    }
+}
---
+++ ChatApp.Mobile/ChatApp.Mobile/ViewModels/ChatRoomPageViewModelViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ChatApp.Mobile.Services.Interfaces;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChatApp.Mobile.ViewModels
+{
+    public class ChatRoomPageViewModelViewModel : ViewModelBase
+    {
+        private readonly IChatService chatService;
+
+        public ChatRoomPageViewModelViewModel(INavigationService navigationService, 
+            IChatService chatService) 
+            : base(navigationService)
+        {
+            this.chatService = chatService;
+        }
+
+        public override async void Initialize(INavigationParameters parameters)
+        {
+            base.Initialize(parameters);
+           await chatService.Connect(); 
+        }
+    }
+}
